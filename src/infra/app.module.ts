e070fa3387caafe554b9c903a331855ee1b05521
Vinicius Barbosa <viniciusmbarbosa@hotmail.com>
2023-09-15 11:48:40 -0300

    feat: criando camada de infraestrutura

---
+++ src/infra/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common'
+import { PrismaService } from './prisma/prisma.service'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
+import { HttpModule } from './http/http.module'
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+    HttpModule,
+  ],
+  providers: [PrismaService],
+})
+export class AppModule {}
