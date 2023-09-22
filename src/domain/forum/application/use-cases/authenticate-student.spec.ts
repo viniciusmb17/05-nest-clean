7f7376c26906e9dadf93cc256255210a6c6321d8
Vinicius Barbosa <viniciusmbarbosa@hotmail.com>
2023-09-22 08:25:36 -0300

    feat: testes do cadastro e autenticação

---
+++ src/domain/forum/application/use-cases/authenticate-student.spec.ts
@@ -0,0 +1,45 @@
+import { FakeEncrypter } from 'test/cryptography/fake-encrypter'
+import { FakeHasher } from 'test/cryptography/fake-hasher'
+import { makeStudent } from 'test/factories/make-student'
+import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository'
+import { AuthenticateStudentUseCase } from './authenticate-student'
+
+let inMemoryStudentsRepository: InMemoryStudentsRepository
+let fakeHasher: FakeHasher
+let fakeEncrypter: FakeEncrypter
+
+let sut: AuthenticateStudentUseCase // System Under Test
+
+describe('Authenticate Student', () => {
+  beforeEach(() => {
+    inMemoryStudentsRepository = new InMemoryStudentsRepository()
+
+    fakeHasher = new FakeHasher()
+    fakeEncrypter = new FakeEncrypter()
+
+    sut = new AuthenticateStudentUseCase(
+      inMemoryStudentsRepository,
+      fakeHasher,
+      fakeEncrypter,
+    )
+  })
+
+  it('should be able to register a new student', async () => {
+    const student = makeStudent({
+      email: 'johndoe@example.com',
+      password: await fakeHasher.hash('123456'),
+    })
+
+    inMemoryStudentsRepository.items.push(student)
+
+    const result = await sut.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toEqual({
+      accessToken: expect.any(String),
+    })
+  })
+})
