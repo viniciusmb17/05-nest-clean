6da7fee58b9beba80b46c9c733d1a4e6323c2edc
Vinicius Barbosa <viniciusmbarbosa@hotmail.com>
2023-10-29 21:23:42 -0300

    fix: imports

--- test/factories/make-student.ts
+++ test/factories/make-student.ts
@@ -1,12 +1,12 @@
-import { faker } from '@faker-js/faker'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import {
   Student,
   StudentProps,
 } from '@/domain/forum/enterprise/entities/student'
-import { Injectable } from '@nestjs/common'
-import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { PrismaStudentMapper } from '@/infra/database/prisma/mappers/prisma-student-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 
 export function makeStudent(
   override: Partial<StudentProps> = {},
