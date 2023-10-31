bfa645472ba1fdeb70c255d86f0159327313cc09
Vinicius Barbosa <viniciusmbarbosa@hotmail.com>
2023-10-31 14:45:31 -0300

    fix: BcryptHasher import

--- src/infra/cryptography/cryptography.module.ts
+++ src/infra/cryptography/cryptography.module.ts
@@ -2,7 +2,7 @@ import { Encrypter } from '@/domain/forum/application/cryptography/encrypter'
 import { HashComparer } from '@/domain/forum/application/cryptography/hash-comparer'
 import { HashGenerator } from '@/domain/forum/application/cryptography/hash-generator'
 import { Module } from '@nestjs/common'
-import { BcryptHasher } from './bcrypt-hashser'
+import { BcryptHasher } from './bcrypt-hasher'
 import { JwtEncrypter } from './jwt-encrypter'
 
 @Module({
